e7eec534ed991f93fe4fd1856a609956143641c4
Bas Beelen <bas@infinic.com>
2023-07-25 16:13:05 +0200

    Completely static

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import "/src/assets/main.css";
-import { createApp } from "/node_modules/.vite/deps/vue.js?v=4b9c414e";
-import App from "/src/App.vue";
-import router from "/src/router/index.ts";
-const app = createApp(App);
-app.use(router);
-app.mount("#app");
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4udHMiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0ICcuL2Fzc2V0cy9tYWluLmNzcydcblxuaW1wb3J0IHsgY3JlYXRlQXBwIH0gZnJvbSAndnVlJ1xuaW1wb3J0IEFwcCBmcm9tICcuL0FwcC52dWUnXG5pbXBvcnQgcm91dGVyIGZyb20gJy4vcm91dGVyJ1xuXG5jb25zdCBhcHAgPSBjcmVhdGVBcHAoQXBwKVxuXG5hcHAudXNlKHJvdXRlcilcblxuYXBwLm1vdW50KCcjYXBwJylcbiJdLCJtYXBwaW5ncyI6IkFBQUEsT0FBTztBQUVQLFNBQVMsaUJBQWlCO0FBQzFCLE9BQU8sU0FBUztBQUNoQixPQUFPLFlBQVk7QUFFbkIsTUFBTSxNQUFNLFVBQVUsR0FBRztBQUV6QixJQUFJLElBQUksTUFBTTtBQUVkLElBQUksTUFBTSxNQUFNOyIsIm5hbWVzIjpbXX0=
